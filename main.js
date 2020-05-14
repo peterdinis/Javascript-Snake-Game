1229453e03308321b7793b1a4f3b388a93627ad4
Zero <dinismatej1@gmail.com>
2020-05-14 02:22:30 +0200

    refactor, dynamic scale

--- main.js
+++ main.js
@@ -1,66 +1,61 @@
 window.onload=function() {
-    canv=document.getElementById("gc");
-    ctx=canv.getContext("2d");
+    Canvas=document.getElementById("gc");
+    CTX=Canvas.getContext("2d");
     document.addEventListener("keydown",keyPush);
     setInterval(game,1000/15);
 }
-px=py=10;
-gs=tc=20;
-ax=ay=15;
-xv=yv=0;
-trail=[];
-tail = 5;
+
+SnakeX=SnakeY=10;
+scale=17;
+AppleX=AppleY=15;
+DirX=DirY=0;
+Body=[];
+DefalutLength = 5;
+
 function game() {
-    px+=xv;
-    py+=yv;
-    if(px<0) {
-        px= tc-1;
-    }
-    if(px>tc-1) {
-        px= 0;
-    }
-    if(py<0) {
-        py= tc-1;
-    }
-    if(py>tc-1) {
-        py= 0;
-    }
-    ctx.fillStyle="black";
-    ctx.fillRect(0,0,canv.width,canv.height);
+    bx=Math.floor(Canvas.width/scale);
+    by=Math.floor(Canvas.height/scale);
+    SnakeX+=DirX;
+    SnakeY+=DirY;
+    if(SnakeX<0) {SnakeX= bx-1;}
+    if(SnakeX>bx-1) {SnakeX= 0;}
+    if(SnakeY<0) {SnakeY= by-1;}
+    if(SnakeY>by-1) {SnakeY= 0;}
 
-    ctx.fillStyle="lime";
-    for(var i=0;i<trail.length;i++) {
-        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
-        if(trail[i].x==px && trail[i].y==py) {
-            tail = 5;
-        }
+    CTX.fillStyle="black";
+    CTX.fillRect(0,0,Canvas.width,Canvas.height);
+
+    CTX.fillStyle="lime";
+    for(var i=0; i<Body.length; i++) {
+        CTX.fillRect(Body[i].x*scale,Body[i].y*scale,scale-2,scale-2);
+        if(Body[i].x===SnakeX && Body[i].y===SnakeY) {DefalutLength = 5;}
     }
-    trail.push({x:px,y:py});
-    while(trail.length>tail) {
-    trail.shift();
+    Body.push({x:SnakeX,y:SnakeY});
+    while(Body.length>DefalutLength) {Body.shift();}
+
+    if(AppleX===SnakeX && AppleY===SnakeY) {
+        DefalutLength++;
+        AppleX=Math.floor(Math.random()*tc);
+        AppleY=Math.floor(Math.random()*tc);
     }
 
-    if(ax==px && ay==py) {
-        tail++;
-        ax=Math.floor(Math.random()*tc);
-        ay=Math.floor(Math.random()*tc);
-    }
-    ctx.fillStyle="red";
-    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+    CTX.fillStyle="red";
+    CTX.fillRect(AppleX*scale,AppleY*scale,scale-2,scale-2);
 }
+
 function keyPush(evt) {
     switch(evt.keyCode) {
         case 37:
-            xv=-1;yv=0;
+            DirX=-1;DirY=0;
             break;
         case 38:
-            xv=0;yv=-1;
+            DirX=0;DirY=-1;
             break;
         case 39:
-            xv=1;yv=0;
+            DirX=1;DirY=0;
             break;
         case 40:
-            xv=0;yv=1;
+            DirX=0;DirY=1;
             break;
     }
 }
